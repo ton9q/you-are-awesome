8f5febbae91eb9ef721eab61524fcde6b88a01d4
ton9q <kuchma_anton@mail.ru>
2018-09-23 23:05:58 +0300

    addiction of logic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,86 @@
-// DO WHATEVER YOU WANT HERE
+const createEnumerableProperty = (property) => {
+  return property;
+};
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createNotEnumerableProperty = (property) => {
+  Object.defineProperty(Object.prototype, property, {
+    value: "value", enumerable: false
+  });
 
-// return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+  return property;
+};
+
+const createProtoMagicObject = () => {
+  function f() {};
+  f.prototype = f.__proto__;
+
+  return f;
+};
+
+var counter1 = 0; // variable for incrementor()
+
+const incrementor = () => {
+  counter1++;
+
+  return incrementor;
+};
+
+incrementor.valueOf = _ => counter1;
+
+var counter2 = 0; // variable for asyncIncrementor()
+
+const asyncIncrementor = () => {
+  return new Promise(resolve => {
+    resolve(++counter2);
+  });
+};
+
+const createIncrementer = () => {
+  function* increment() {
+    var value = 0;
+
+    while(value < 10) {
+      yield ++value;
+    }
+  } 
+
+  return increment();
+};
+
+const returnBackInSecond = (param) => {
+  return new Promise(resolve => {
+    setTimeout( _ => {
+      resolve(param);
+    }, 1000);
+  });
+};
+
+const getDeepPropertiesCount = (obj) => {
+  let count = Object.keys(obj).length;
+
+  for (let property in obj) {
+    if (typeof obj[property] === "object"){
+      count += getDeepPropertiesCount(obj[property]);
+    }
+  }
+
+  return count;
+};
+
+const createSerializedObject = () => {
+  return {name: "Anton", surname: "Kuchma",
+    toString: function() {
+      return this.name + " " + this.surname;
+    }, 
+    toJSON: function() {
+      return this.name + " " + this.surname;
+    }
+  };
+};
+
+const sortByProto = (array) => {
+  return array.sort((a, b) => a - b);
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
